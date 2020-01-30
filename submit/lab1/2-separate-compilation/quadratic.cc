16578f80c5d204410495e6440b400ac4d61d5393
Berk Cohadar <bcohada1@cs.binghamton.edu>
2020-01-30 18:42:13 -0500

    completing lab1

---
+++ submit/lab1/2-separate-compilation/quadratic.cc
@@ -0,0 +1,9 @@
+#include "fns-compare.hh"
+
+static double
+quadratic(double n) {
+	const double C = 1000;
+	return C*n*n;
+}
+
+static const bool isRegistered = register_fn(FnInfo("quadratic", quadratic));
