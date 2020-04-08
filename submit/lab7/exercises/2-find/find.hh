fa095ec92e22572222e98eab71ff614a337d331c
berkcohadar <berkcohadar1@gmail.com>
2020-04-08 16:57:30 -0400

    completing lab7

---
+++ submit/lab7/exercises/2-find/find.hh
@@ -0,0 +1,34 @@
+#ifndef OUT_HH_
+#define OUT_HH_
+
+#include <vector>
+
+/** Output a line containing "FOUND" if arg is found in [begin, end),
+ *  otherwise output "NOT FOUND"..
+ */
+template <typename Iterator>
+void loopFn(Iterator begin, Iterator end, int arg) {
+  //@TODO
+	for (auto p = begin; p!= end; ++p)
+	{
+		if(*p == arg) {
+			std::cout << "FOUND" << std::endl;
+			return;
+		}
+	}
+	std::cout << "NOT FOUND" << std::endl;
+
+}
+
+/** Exactly like loopFn() but no loops should be used */
+template <typename Iterator>
+void noLoopFn(Iterator begin, Iterator end, int arg) {
+  //@TODO
+	if(std::find(begin,end,arg) != end) if(*(std::find(begin,end,arg)) == arg) {
+		std::cout << "FOUND" << std::endl;
+		return;
+	}
+	std::cout << "NOT FOUND" << std::endl;
+}
+
+#endif //ifndef OUT_HH_
